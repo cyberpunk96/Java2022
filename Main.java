7e3d853b1fcaf26c152a7ffe5f0031156fa5e5af
Yavuz Ünlükaya <72576186+cyberpunk96@users.noreply.github.com>
2023-03-28 02:17:34 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		char grade = 'E';
+
+		switch (grade) {
+
+		case 'A':
+			System.out.println("Mükemmel : Geçtiniz");
+			break;
+
+		case 'B':
+			System.out.println("Çok Güzel : Geçtiniz");
+			break;
+
+		case 'C':
+			System.out.println("iyi : Geçtiniz");
+			break;
+		case 'D':
+			System.out.println("Orta : Geçtiniz");
+			break;
+		case 'F':
+		case 'E':
+			System.out.println("Malesef Kaldınız :(");
+			break;
+		default:
+			System.out.println("Geçersiz Not Girdiniz");
+
+		}
+
+	}
+
+}
